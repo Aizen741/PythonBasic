2e35ee1f2adc43d878253cb6f131557d409bdca5
Aizen741 <rahulsivadas2009@gmail.com>
2019-06-24 21:17:20 +0530

    PythonBasic project

---
+++ ascending_and_decending_order.py
@@ -0,0 +1,13 @@
+#Using quick Sort
+
+data = [22 , 1, 33, 44, 5, 223, 4]
+data.sort()
+
+#dor Ascending order
+
+print("Ascending order : ", data)
+
+#for Decending order
+
+data.sort(reverse=True)
+print("Decending order: ", data)
