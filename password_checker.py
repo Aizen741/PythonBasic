297652cce3f11d7571a677a3ca6c261975f2b937
Aizen741 <rahulsivadas2009@gmail.com>
2019-07-06 08:49:58 +0530

    Pass

--- password_checker.py
+++ password_checker.py
@@ -1,8 +1,11 @@
 correct_password = "1234"
-
+name = input("Enter your name:")
 password = input("Enter the password here: ")
 
 while correct_password != password:
-    password = input("Wrong password Enter again:")
 
-print("Logged in.")
+    password = input("Wrong password please Enter again:")
+
+message = "Hi %s you are logged in" % name
+print(message)
+
