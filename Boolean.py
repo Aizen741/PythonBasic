2a18d636b1924c5dd722952469694431fc746580
Aizen741 <rahulsivadas2009@gmail.com>
2019-07-09 14:18:50 +0530

    Boolean_ex

---
+++ Boolean.py
@@ -0,0 +1,9 @@
+a = input("Enter first value:")
+b = input("Enter the second value:")
+c = a == b
+def myfunc(c):
+    if c == True:
+        return 'It is Boolean'
+    else:
+        return 'It is not Boolean'
+print(myfunc(c))
